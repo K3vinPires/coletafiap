85e4ca922d2bdf883d40d1745903d7b737c76022
Kevin <kevinpiresdeoliveira@gmail.com>
2024-10-13 19:17:14 -0300

    commit inicial

---
+++ ColetaApi/Migrations/20240625221803_01-Coleta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ColetaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class _01Coleta : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "COLETA",
+                columns: table => new
+                {
+                    Pk_id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Ds_coleta = table.Column<string>(type: "NVARCHAR2(50)", maxLength: 50, nullable: false),
+                    Ds_tipocoleta = table.Column<string>(type: "NVARCHAR2(50)", maxLength: 50, nullable: false),
+                    Dt_coleta = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_COLETA", x => x.Pk_id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_COLETA_Ds_coleta",
+                table: "COLETA",
+                column: "Ds_coleta");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "COLETA");
+        }
+    }
+}
---
+++ coleta.teste/Testes/ColetaTeste.cs
@@ -0,0 +1,43 @@
+﻿using ColetaApi.Controllers;
+using ColetaApi.Data.Contexts;
+using ColetaApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ColetaApi.Tests
+{
+    public class ColetaControllerTests : IClassFixture<WebApplicationFactory<ColetaApi.Startup>>
+    {
+        private readonly WebApplicationFactory<ColetaApi.Startup> _factory;
+
+        public ColetaControllerTests(WebApplicationFactory<ColetaApi.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/Coleta/Index")]
+        [InlineData("/Coleta/Create")]
+        [InlineData("/Coleta/Update/1")]
+        [InlineData("/Coleta/Delete/1")]
+        public async Task Get_EndpointsReturnSuccessAnd200Status(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
